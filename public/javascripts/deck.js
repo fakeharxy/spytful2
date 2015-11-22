89ba9f946a01725fcff83ab41da56b69da7f83a5
Harxy <harkerp@gmail.com>
2015-11-22 22:17:30 +0000

    Add a logo for the back of the cards, and end the game when the deck runs out

--- public/javascripts/deck.js
+++ public/javascripts/deck.js
@@ -1,6 +1,6 @@
 var Deck = {
-    cardArray : [],
-    deckSizeMultiple : 3,
+    cardArray: [],
+    deckSizeMultiple: 3,
     cardWidth: 60,
     cardHeight: 100,
     cardRoundingRadius: 10,
@@ -9,27 +9,39 @@ var Deck = {
     cardWobbleMax: 8,
     drawPoints: [],
 
-    deal: function (dealLocation, dealNumber){
-        Array.prototype.push.apply(dealLocation, this.cardArray.splice(0, dealNumber));
+    deal: function(dealLocation, dealNumber) {
+        Array.prototype.push.apply(dealLocation, this.cardArray.splice(
+            0, dealNumber));
+        if (this.cardArray.length === 0) {
+            alert('This game is over. The winner is Player 1');
+        }
     },
 
-    buildDeck: function (hexArray) {
-        this.cardPool=[];
-        this.cardArray=[];
-        var points = [ [0,0], [Deck.cardWidth,0], [Deck.cardWidth,Deck.cardHeight], [0,Deck.cardHeight] ];
+    buildDeck: function(hexArray) {
+        this.cardPool = [];
+        this.cardArray = [];
+        var points = [
+            [0, 0],
+            [Deck.cardWidth, 0],
+            [Deck.cardWidth, Deck.cardHeight],
+            [0, Deck.cardHeight]
+        ];
         drawPoints = Board.getRoundedPoints(points, Deck.cardRoundingRadius);
 
-        for (var j=0 ; j < hexArray.length ; j++) {
-            for (var i=0 ; i < hexArray[j].length; i++) {
-                for (var n=0 ; n < this.deckSizeMultiple ; n++) {
-                    if (hexArray[j][i].colourCode!=0) {
+        for (var j = 0; j < hexArray.length; j++) {
+            for (var i = 0; i < hexArray[j].length; i++) {
+                for (var n = 0; n < this.deckSizeMultiple; n++) {
+                    if (hexArray[j][i].colourCode != 0) {
                         var card = Object.create(Card);
                         card.colourCode = hexArray[j][i].colourCode;
                         card.regionName = hexArray[j][i].regionName;
-                        card.rotation = Math.random() * Deck.cardRotationMax - Deck.cardRotationMax / 2;
+                        card.rotation = Math.random() * Deck.cardRotationMax -
+                            Deck.cardRotationMax / 2;
                         var wobble = {};
-                        wobble.x = Math.random() * Deck.cardWobbleMax - Deck.cardWobbleMax / 2;
-                        wobble.y = Math.random() * Deck.cardWobbleMax - Deck.cardWobbleMax / 2;
+                        wobble.x = Math.random() * Deck.cardWobbleMax -
+                            Deck.cardWobbleMax / 2;
+                        wobble.y = Math.random() * Deck.cardWobbleMax -
+                            Deck.cardWobbleMax / 2;
                         card.wobble = wobble;
                         this.cardArray.push(card);
                     }
@@ -38,23 +50,26 @@ var Deck = {
         }
     },
 
-    shuffle: function(o){
-        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    shuffle: function(o) {
+        for (var j, x, i = o.length; i; j = Math.floor(Math.random() *
+                i), x = o[--i], o[i] = o[j], o[j] = x);
         return o;
     },
 
-    draw: function (ctx, x, y) {
+    draw: function(ctx, x, y) {
         //draw card pool
-        if (this.cardPool.length>0) {
+        if (this.cardPool.length > 0) {
             this.cardPool[0].draw(ctx, x, y, true);
-            if (this.cardPool.length>1) {
-                this.cardPool[1].draw(ctx, x + Deck.cardWidth + Deck.cardSpacing, y, true);
+            if (this.cardPool.length > 1) {
+                this.cardPool[1].draw(ctx, x + Deck.cardWidth +
+                    Deck.cardSpacing,
+                    y, true);
             }
         }
 
         //draw deck
-        var xHeap = x + 2*(Deck.cardWidth + Deck.cardSpacing);
-        for (var i=this.cardArray.length-1;i>=0;i--) {
+        var xHeap = x + 2 * (Deck.cardWidth + Deck.cardSpacing);
+        for (var i = this.cardArray.length - 1; i >= 0; i--) {
             this.cardArray[i].draw(ctx, xHeap, y, false);
         }
     }
@@ -62,7 +77,7 @@ var Deck = {
 
 var Card = {
 
-    draw: function (ctx, x, y, faceUp) {
+    draw: function(ctx, x, y, faceUp) {
         ctx.save();
 
         ctx.translate(x + this.wobble.x, y + this.wobble.y);
@@ -83,16 +98,27 @@ var Card = {
 
         ctx.lineWidth = 2;
         ctx.stroke();
-        ctx.fillStyle = faceUp ? Hex.colourMap[this.colourCode] : "#FFF";
+        ctx.fillStyle = faceUp ? Hex.colourMap[this.colourCode] :
+            "#FFF";
         ctx.fill();
 
         //write region name
-        //if (faceUp) {
-        ctx.fillStyle = "#000"
-        ctx.textBaseline = "middle";
-        ctx.textAlign = "center";
-        ctx.fillText(faceUp ? this.regionName : "LOGO", Deck.cardWidth/2, Deck.cardHeight/2);
-        //}
+        if (faceUp) {
+            ctx.fillStyle = "#000"
+            ctx.textBaseline = "middle";
+            ctx.textAlign = "center";
+            ctx.fillText(
+                this.regionName,
+                Deck.cardWidth / 2,
+                Deck.cardHeight / 2);
+        } else {
+            ctx.drawImage(
+                ctx.imageCache['logo'],
+                2.5,
+                2.5,
+                Deck.cardWidth - 5,
+                Deck.cardHeight - 5);
+        }
         ctx.restore();
     }
 };
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,45 +1,55 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var canvas = $("#canvas")[0];
-  ctx = canvas.getContext("2d");
-  w = $("#canvas").width();
-  h = $("#canvas").height();
+    var canvas = $("#canvas")[0];
+    ctx = canvas.getContext("2d");
+    w = $("#canvas").width();
+    h = $("#canvas").height();
 
-  ctx.font = "8pt Arial";
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
-  ctx.fillText("loading...", w/2, h/2);
+    ctx.font = "8pt Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText("loading...", w / 2, h / 2);
 
-  ctx.imageCache = [];
-  var imgList = [ { name: "water", file: "water.gif" },
-	  { name: "briefcase", file: "briefcase.png" },
-    { name: "extractionpoint", file: "exit.png"}];
+    ctx.imageCache = [];
+    var imgList = [{
+        name: "water",
+        file: "water.gif"
+    }, {
+        name: "briefcase",
+        file: "briefcase.png"
+    }, {
+        name: "extractionpoint",
+        file: "exit.png"
+    }, {
+        name: 'logo',
+        file: "logo.png"
+    }];
 
-  imagesToLoad = imgList.length;
-  for (var i=0; i<imgList.length; i++) {
-	  loadImage(imgList[i]);
-  }
+    imagesToLoad = imgList.length;
+    for (var i = 0; i < imgList.length; i++) {
+        loadImage(imgList[i]);
+    }
 
-  var butAction = $("#butAction")[0];
-  butAction.onclick = actionFunction;
+    var butAction = $("#butAction")[0];
+    butAction.onclick = actionFunction;
 });
 
 function loadImage(imgToLoad) {
-	var img = new Image;
-	img.onload = function() {
+    var img = new Image;
+    img.onload = function() {
         console.log("loaded image " + imgToLoad.file);
-		if (--imagesToLoad==0) imagesReady();
-	};
-	img.onerror = function() {
-		console.log("error loading image " + imgToLoad.file);
-	};
-	img.src = "images/" + imgToLoad.file;
-	ctx.imageCache[imgToLoad.name] = img;
+        if (--imagesToLoad == 0) imagesReady();
+    };
+    img.onerror = function() {
+        console.log("error loading image " + imgToLoad.file);
+    };
+    img.src = "images/" + imgToLoad.file;
+    ctx.imageCache[imgToLoad.name] = img;
 }
 
 function imagesReady() {
-  setup();
-  draw();
+    setup();
+    draw();
 }
 
 var ctx, w, h;
@@ -47,25 +57,27 @@ var imagesToLoad;
 var game;
 
 function setup() {
-  game = Object.create(Game);
-  game.setup(10,10);
-  game.addPlayer("Player 1");
-  game.prepareGame();
+    game = Object.create(Game);
+    game.setup(10, 10);
+    game.addPlayer("Player 1");
+    game.prepareGame();
 };
 
 function draw() {
-  //reset canvas
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, w, h);
-  ctx.strokeStyle = "black";
-  ctx.strokeRect(0, 0, w, h);
+    //reset canvas
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, w, h);
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(0, 0, w, h);
 
-  //draw objects
-  game.draw(ctx);
+    //draw objects
+    game.draw(ctx);
 };
 
 function actionFunction() {
-  //deals another card to random player
-  game.deck.deal(game.players[Math.floor(Math.random() * game.players.length)].hand, 1);
-  draw();
+    //deals another card to random player
+    game.deck.deal(game.players[Math.floor(Math.random() * game.players
+            .length)]
+        .hand, 1);
+    draw();
 }
