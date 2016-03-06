eaeba3989abcc25512e420dc798c8386fcfa2507
Harxy <harkerp@gmail.com>
2016-03-06 12:12:30 +0000

    Add beginning of chat feature

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/public'));
-
-app.listen(process.env.PORT || 3080, function () {
-  console.log('Spytful is running on port 3080!');
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/public/index.html');
 });
 
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(process.env.PORT || 3000, function() {
+  console.log('listening on *:3000');
+});
