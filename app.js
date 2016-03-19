346b8fed6f7283b416ded74b8109c551ebd0c96b
Thomas Nicholson <thomasenicholson@hotmail.com>
2016-03-19 22:42:42 +0100

    adds player names

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var session = Session({
       resave: true,
       saveUninitialized: true
     });
-    
+        
+var players = [];
 
 app.use(session);
 
@@ -34,26 +35,46 @@ io.use(function(socket, next) {
 io.on('connection', function(socket) {
   var uid = socket.handshake.session.uid;
   console.log('io connection started using session id ' + uid);
+  if (players[uid]) { //check the player is still in memory
  
-  socket.on('lobby', function(msg) {
-    if (msg.indexOf('sweary')==-1) {
-      console.log('message from client ' + uid + ': ' + msg);
-      io.emit('lobby', msg);
-    } else {
-      console.log('client ' + uid + ' swore');
-      socket.emit('lobby', 'message rejected you rudey');
-    }
-  });
-  
-  socket.on('game', function(msg) {
-    var uid = socket.handshake.session.uid;
-    console.log('game message from client ' + uid + ': ' + msg);
-    io.emit('game', msg);
-  });
-  
-  socket.on('disconnect', function() {
-    console.log('io disconnect with session id ' + socket.handshake.session.uid);
-  });
+    socket.on('setname', function(newname) {
+      var uid = this.handshake.session.uid;
+      var oldname = players[uid].name;
+      console.log('setname from uid ' + uid + ' (' + oldname + ' -> ' + newname + ')');
+      players[uid].name = newname;
+      io.emit('lobby', oldname + ' changed name to ' + newname);
+    });
+    
+    socket.on('getname', function() {
+      var uid = this.handshake.session.uid;
+      console.log('getname from uid ' + uid + ' (' + players[uid].name + ')');
+      socket.emit('setname', players[uid].name);
+    });
+    
+    socket.on('lobby', function(msg) {
+      var uid = this.handshake.session.uid;
+      if (msg.indexOf('sweary')==-1) {
+        console.log('lobby message from client ' + uid + ' (' + players[uid].name + ') : ' + msg);
+        io.emit('lobby', players[uid].name + ': ' + msg);
+      } else {
+        console.log('client ' + uid + ' swore');
+        socket.emit('lobby', 'message rejected you rudey');
+      }
+    });
+    
+    socket.on('game', function(msg) {
+      var uid = this.handshake.session.uid;
+      console.log('game message from client ' + uid + ' (' + players[uid].name + '): ' + msg);
+      io.emit('game', players[uid].name + ': ' + msg);
+    });
+    
+    socket.on('disconnect', function() {
+      console.log('io disconnect with session id ' + this.handshake.session.uid);
+    });
+    
+  } else {
+    console.log("(session no longer active; connection ignored)");
+  }
 });
 
 http.listen(process.env.PORT || 3000, function() {
@@ -61,10 +82,13 @@ http.listen(process.env.PORT || 3000, function() {
 });
 
 function checkSessionId(req) {
-  if (req.session.uid) {
-    console.log('request using active session ' + req.session.uid);
+  if (req.session.uid && players[req.session.uid]) {
+    console.log('request using active session ' + req.session.uid + " (" + players[req.session.uid].name + ")");
   } else {
-    req.session.uid = Date.now();
-    console.log('new session! assigned id ' + req.session.uid);
+    var uid = Date.now();
+    req.session.uid = uid;
+    var newplayer = { name: "annnnnonnn" };
+    players[uid] = newplayer;
+    console.log('new session! assigned id ' + uid);
   }
 }
